b5cb00f01830b51759f3c1afe0030cc480d9d512
Daniel Eriksson <daniel.eke.eriksson@gmail.com>
2016-11-04 14:28:40 +0100

    this commit contains viewclass tests

---
+++ src/test/java/com/assignment07/Test/ViewClassTest.java
@@ -0,0 +1,14 @@
+package com.assignment07.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ViewClassTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
